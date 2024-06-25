e686a5617f014e8d49112855f8e55d69b061e12e
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-06-25 17:33:58 -0300

    subir questão 1

---
+++ questao1.java
@@ -0,0 +1,15 @@
+package questionarioaula2;
+
+public class questao1 {
+    public static void main(String[] args) {
+        //1- Questão Escreva um programa que verifica se um número é positivo, negativo ou zero.
+        int numero = 5;
+        if (numero > 0) {
+            System.out.println("O numero é positivo");
+        } else if (numero < 0) {
+            System.out.println("O numero é negativo");
+        } else {
+            System.out.println("O numero é zero");
+        }
+    }
+}
